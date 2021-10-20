87e526fe2f0895da2ed0976f2fbc703bc4efc00d
BrunoUemura <bruno.h.uemura@outlook.com>
2021-10-20 20:00:35 -0300

    Done project

--- src/server.js
+++ src/server.js
@@ -1,27 +1,30 @@
 const express = require("express");
 const app = express();
-const router = require("./routes");
+const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send({ message: "Backend API" });
-});
+const port = process.env.PORT || 5000;
 
+// middleware
+app.use(express.static("./public"));
 app.use(express.json());
-app.use("/api/v1/tasks", router);
 
-const port = 3000;
+// routes
+app.use("/api/v1/tasks", tasks);
 
-const start = async () => {
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+(async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, () => {
-      console.log(`Server running at port ${port}...`);
-    });
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
   } catch (error) {
     console.log(error);
   }
-};
-
-start();
+})();
